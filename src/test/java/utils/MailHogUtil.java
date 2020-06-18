29c2528cd30b25d65228ced2bbc3e43e52a1da2b
Gleb Garunchik <garunchikgleb@gmail.com>
2020-06-19 02:40:31 +0300

    TSM-004: refactored code

--- src/test/java/utils/MailHogUtil.java
+++ src/test/java/utils/MailHogUtil.java
@@ -3,10 +3,9 @@ package utils;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.ex.ElementShould;
 import components.EmailComponent;
 import io.qameta.allure.Step;
-import org.testng.Assert;
+import org.openqa.selenium.By;
 
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -19,7 +18,7 @@ public class MailHogUtil {
     private static final String MAILHOG_URL = "https://autotest.leverice.net/email/#";
     private static final String EMAIL_LIST_CSS = ".msglist-message";
     private static final String TOOLBAR_CSS = ".toolbar";
-    private static final String VALIDATION_CODE_IFRAME_ID = ".preview-html";
+    private static final String VALIDATION_CODE_IFRAME_ID = "preview-html";
     private static final String VALIDATION_CODE_CSS = ".validation-code";
     private static Map<Integer, EmailComponent> emailComponentMap = new LinkedHashMap<>();
 
@@ -32,47 +31,42 @@ public class MailHogUtil {
     }
 
     public static void isTabOpened() {
-        try {
-            $(TOOLBAR_CSS).waitUntil(Condition.visible, 3000);
-            AllureUtils.takeScreenshot();
-        } catch (ElementShould ex) {
-            Assert.fail("Page has not been load");
-            AllureUtils.takeScreenshot();
-        }
+        $(TOOLBAR_CSS).waitUntil(Condition.visible, 3000);
+        AllureUtils.takeScreenshot();
     }
 
     @Step("Getting all emails in a list")
     public static void getAllEmails() {
         List<SelenideElement> emailList = $$(EMAIL_LIST_CSS);
         List<SelenideElement> emailNamesList = $$(EmailComponent.getEmailToClassname());
+        List<SelenideElement> subjectNameList = $$(EmailComponent.getSubjectClassname());
         for (int i = 0; i < emailList.size(); i++) {
-            emailComponentMap.put(i, new EmailComponent(emailNamesList.get(i)));
+            emailComponentMap.put(i,
+                    new EmailComponent(
+                            emailNamesList.get(i),
+                            subjectNameList.get(i)));
         }
     }
 
-    @Step("Opening first matching to '{emailName}' email in the list")
-    public static void openEmailByName(String emailName) {
-        try {
-            for (int i = 0; i < emailComponentMap.size(); i++) {
-                if (emailComponentMap.get(i).getEmailToName().equals(emailName)) {
-                    emailComponentMap.get(i).clickOnEmail();
-                }
+    @Step("Opening first matching to '{emailName}' email with subject '{subject}' in the list")
+    public static void openEmailByNameAndASubject(String emailName, String subject) {
+        for (int i = 0; i < emailComponentMap.size(); i++) {
+            if (emailComponentMap.get(i).getEmailToName().equals(emailName) & emailComponentMap.get(i).getSubjectName().equals(subject)) {
+                emailComponentMap.get(i).clickOnEmail();
             }
-        } catch (ElementShould elementShould) {
-            Assert.fail("No such email on the page");
-            AllureUtils.takeScreenshot();
         }
     }
 
     @Step("Getting verification code from '{emailName}'")
-    public String getValidationCode(String emailName) {
+    public static String getValidationCode(String emailName) {
         openTab();
         getAllEmails();
-        openEmailByName(emailName);
-        switchTo().frame($(VALIDATION_CODE_IFRAME_ID));
+        openEmailByNameAndASubject(emailName, "Leverice email validation");
+        switchTo().frame($(By.id(VALIDATION_CODE_IFRAME_ID)));
         AllureUtils.takeScreenshot();
+        String validationCode = $(VALIDATION_CODE_CSS).getText().trim();
         closeTab();
-        return $(VALIDATION_CODE_CSS).getText().trim();
+        return validationCode;
     }
 
     @Step("Closing MailHog tab")
