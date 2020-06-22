7daad9574194ca253a324c81af3df1d56e41695a
Gleb Garunchik <garunchikgleb@gmail.com>
2020-06-22 15:17:43 +0300

    TSM-005: added random number to cover case where already registered user stucked in a flow

--- src/test/java/tests/MainMenuTest.java
+++ src/test/java/tests/MainMenuTest.java
@@ -3,12 +3,16 @@ package tests;
 import org.testng.annotations.Test;
 import tests.base.BaseTest;
 
+import java.util.Random;
+
 public class MainMenuTest extends BaseTest {
+
+    private static final String randomNumber = String.valueOf(Math.random() * ((999 - 1) + 1)) + 1;
 
     @Test(description = "Verifying user can create a new workspace")
     public void verifyUserCanCreateWorkspace() {
         mainMenuSteps.createNewWorkspace(
-                "wonder@day.com",
+                "wonder" + randomNumber + "@day.com",
                 "TMS-Name",
                 "TMS-Surname",
                 "TMS-Workspace");
