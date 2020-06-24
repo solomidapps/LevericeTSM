5a59d504cf10d81ab6a1b0f8a5b4defdae04f40c
ggarunchik <garunchikgleb@gmail.com>
2020-06-24 20:30:37 +0300

    bug fix - work in progress

--- src/test/java/steps/CreateChatsSteps.java
+++ src/test/java/steps/CreateChatsSteps.java
@@ -1,8 +1,11 @@
 package steps;
 
+import io.qameta.allure.Step;
 import pages.addNewTopic.CreateTopicChannelPage;
 import pages.createDirectChannelPage.CreateDirectChannelPage;
 import pages.mainMenuPage.*;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class CreateChatsSteps {
     private MainMenuModal mainMenuModal;
@@ -13,6 +16,7 @@ public class CreateChatsSteps {
     private CreateWorkspaceModal createWorkspaceModal;
     private CreateTopicChannelPage topicChannel;
     private CreateDirectChannelPage createDirectChannelPage;
+    private MainMenuSteps mainMenuSteps;
 
     public CreateChatsSteps() {
         mainMenuModal = new MainMenuModal();
@@ -23,10 +27,16 @@ public class CreateChatsSteps {
         createWorkspaceModal = new CreateWorkspaceModal();
         topicChannel = new CreateTopicChannelPage();
         createDirectChannelPage = new CreateDirectChannelPage();
+        mainMenuSteps = new MainMenuSteps();
     }
 
-    public CreateChatsSteps createDirectChannel() {
-
+    @Step("Creating direct channel")
+    public CreateChatsSteps createDirectChannel(String emailToSignIn, String userName) {
+        mainMenuSteps.verifyUserCanSignIn(emailToSignIn);
+        getWebDriver().quit();
+        mainMenuModal
+                .openPage();
+        createDirectChannelPage.createDirectChannel(userName);
         return this;
     }
 }
